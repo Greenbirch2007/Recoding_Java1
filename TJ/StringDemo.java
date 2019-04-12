7d28983e49c7e4e84ea37217ca27efb54dd80f97
Greenbirch2007 <291109028@qq.com>
2019-04-12 17:11:47 +0800

    java数组

---
+++ TJ/StringDemo.java
@@ -0,0 +1,7 @@
+public class StringDemo{
+	public static void main(String args[]) {
+		char[] helloArray = {'h','e','l','~'};
+		String hellString = new String(helloArray);
+		System.out.println(hellString);
+				}
+}
---
+++ TJ/Test.java
@@ -0,0 +1,7 @@
+public class Test{
+	public static void main(String args[]) {
+		StringBuffer sBuffer = new StringBuffer("Test");
+		sBuffer.append("String Bullll");
+		System.out.println(sBuffer);
+	}
+}
