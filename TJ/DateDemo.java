28fc63eeaedd63593dc8c144a4b420287d8c6031
Greenbirch2007 <291109028@qq.com>
2019-04-16 11:35:03 +0800

    捕获组

--- TJ/DateDemo.java
+++ TJ/DateDemo.java
@@ -1,10 +1,26 @@
-import java.util.Date;
+import java.util.*;
+import java.text.*;
+
 
 
 public class DateDemo{
 	public static void main(String args[]) {
-		Date date = new Date();
+		SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
 		
-		System.out.println(date.toString());
+		
+		String input = args.length == 0 ? "1818-11-11" :args[0];
+		
+		System.out.print(input+"Parse as");
+		
+		
+		Date t;
+			
+			try {
+				t = ft.parse(input);
+				System.out.println();
+							}
+		catch (ParseException e ) {
+			System.out.println("Unparseable using" +ft);
+		}
 	}
 }
