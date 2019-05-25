296565bb9c0c2c73664670cea5b5947dd12aa6dc
Greenbirch2007 <291109028@qq.com>
2019-05-25 17:49:15 +0800

    开始加速java

--- TJ/StringDemo.java
+++
@@ -1,7 +0,0 @@
-public class StringDemo{
-	public static void main(String args[]) {
-		char[] helloArray = {'h','e','l','~'};
-		String hellString = new String(helloArray);
-		System.out.println(hellString);
-				}
-}
